418655cdbb83bbe8764ad84add629d1a2c576a92
blonded99 <didls2654@naver.com>
2023-02-05 05:46:00 +0900

    [#3] recyclerview search test1

--- app/src/main/java/com/example/testforissue/MainActivity.kt
+++ app/src/main/java/com/example/testforissue/MainActivity.kt
@@ -2,13 +2,27 @@ package com.example.testforissue
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.testforissue.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        println("hello")
-        println("hello2")
+        val list = ArrayList<String>()
+        list.add("서울")
+        list.add("대전")
+        list.add("대구")
+        list.add("부산")
+        list.add("제주")
+        list.add("일본")
+        binding.recyclerView.adapter = RecyclerViewAdapter(applicationContext,list)
+        binding.recyclerView.layoutManager = LinearLayoutManager(applicationContext)
+
     }
 }
---
+++ app/src/main/java/com/example/testforissue/RecyclerViewAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.testforissue
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewAdapter(val context: Context?, val list: ArrayList<String>):
+    RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_view,
+            parent, false)
+//        val inflater = LayoutInflater.from(parent.context)
+//        val binding = ItemViewBinding.inflate(inflater,parent,false)
+        return RecyclerViewViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerViewViewHolder, position: Int) {
+        holder.setContents(position)
+    }
+
+    override fun getItemCount(): Int {
+        // 검색결과에 표시될 item 개수
+        return list.size
+    }
+
+    inner class RecyclerViewViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val SearchResult: TextView = itemView.findViewById(R.id.textView)
+
+        fun setContents(pos: Int){
+            with(pos){
+                //세팅
+//                SearchResult.setImageResource(R.drawable.trailer_search_sample_image)
+                SearchResult.text = list[pos]
+            }
+
+            /*
+
+            trailerSearchResult.setOnClickListener {
+                이런식으로 clickListener가 여기 있어야 하지만 이게 이제 MVVM 쓰면 xml로 들어가는거
+            }
+
+            */
+        }
+    }
+}
